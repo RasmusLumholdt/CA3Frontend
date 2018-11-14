39f29a7eeac58429ec8417df0dfa5330ee01bd60
MartinMoller <martinmoellerzbc@gmail.com>
2018-11-14 12:48:20 +0100

    added login functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import LogIn from './LogIn';
+import facade from './apiFacade';
+import LoggedIn from './LoggedIn';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { LoggedIn: false }
+  }
+  logout = () => {
+    facade.logout();
+    this.setState({ loggedIn: false });
+  }
+  login = (user, pass) => {
+    facade.login(user, pass)
+      .then(res => this.setState({ loggedIn: true }));
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {!this.state.loggedIn ? (<LogIn login={this.login} />) :
+          (<div>
+            <LoggedIn />
+            <button onClick={this.logout}>Logout</button>
+          </div>)}
       </div>
     );
   }
---
+++ src/LoggedIn.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react"
+import facade from './apiFacade';
+
+class LoggedIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { dataFromServer: "Fetching!!" };
+  }
+  componentDidMount() {
+    facade.fetchData().then(res => this.setState({ dataFromServer: res }));
+  }
+  render() {
+    return (
+      <div>
+        <h2>Data Received from server</h2>
+        <h3>{this.state.dataFromServer}</h3>
+      </div>
+    )
+  }
+}
+
+export default LoggedIn;
